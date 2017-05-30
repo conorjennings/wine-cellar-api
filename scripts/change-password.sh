af3cbed603dcacff2b365b85672faf8f2028cbfb
“conorjennings” <conorjennings@gmail.com>
2017-05-30 16:28:51 -0400

    Modified routes file for wine route.
    Also modified curl script to validate change password.

--- scripts/change-password.sh
+++ scripts/change-password.sh
@@ -1,4 +1,10 @@
+
 #!/bin/bash
+# sh scripts/change-password.sh
+TOKEN='BAhJIiU1NGYyYjQ1ZjhmYTg4ZDg1YmZhZjVhZjMwYWFhNDliYQY6BkVG--53c238844ac4e4557766b8185b889caf349e51c9'
+OLDPW='a'
+NEWPW='w'
+ID='8'
 
 API="${API_ORIGIN:-http://localhost:4741}"
 URL_PATH="/change-password"
